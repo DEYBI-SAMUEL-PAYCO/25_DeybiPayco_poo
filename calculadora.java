4ad7b8dd68051456209767d7527207b0effb98ed
DEYBI-SAMUEL-PAYCO <140242116+DEYBI-SAMUEL-PAYCO@users.noreply.github.com>
2023-09-05 22:29:36 -0500

    Update calculadora.java

--- calculadora.java
+++ calculadora.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,23 @@
  * @author PIERO GAEL
  */
 public class calculadora {
-    
+    //atributos 
+    int a;
+    int b;
+    int mul;
+    int suma;
+    int resta;
+    int dividir ;
+//metodos   
+     public void ingresarDatos() {
+         Scanner entrada = new Scanner (System.in);
+         System.out.println("digitar el primer numero");
+        a = entrada.nextInt();
+         System.out.println("digitar el segundo numero");
+        b = entrada.nextInt();
+    }
+    public void multiplicar() {
+        mul = a * b;
+         System.out.println("la multiplicación es;" + mul);
+    }
 }
